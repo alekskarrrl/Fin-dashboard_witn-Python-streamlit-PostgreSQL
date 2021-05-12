bf0f6e07afd4500b0b55ac985c73683c5e841c72
alekskarrrl <alekskarrrl@gmail.com>
2021-05-13 02:49:53 +0300

    Uploading scripts and .csv files

---
+++ populate_etfs.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 12 23:11:49 2021
+
+@author: User
+"""
+
+import config
+import psycopg2
+import psycopg2.extras
+import csv
+
+
+
+connection = psycopg2.connect(host =config.DB_HOST, database = config.DB_NAME, user = config.DB_USER, password = config.DB_PASS)
+cursor = connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
+cursor.execute("SELECT * FROM stock WHERE is_etf = TRUE")
+
+etfs = cursor.fetchall()
+dates = ['2021-01-25', '2021-01-26', '2021-05-12']
+
+
+for current_date in dates:
+    for etf in etfs:
+        print(etf['symbol'])
+        with open(f"data/{current_date}/{etf['symbol']}.csv") as f:
+            reader = csv.reader(f)
+            next(reader)
+            for row in reader:
+                ticker = row[3]
+                
+                    
+                if ticker:
+                    shares = row[5]
+                    weight = row[7]
+                    cursor.execute("""
+                                   SELECT * FROM stock WHERE symbol=%s
+                                   
+                                   """, (ticker,))
+                    stock = cursor.fetchone()
+                    if stock:
+                        cursor.execute("""
+                                       INSERT INTO etf_holding (etf_id, holding_id, dt, shares, weight)
+                                       VALUES (%s, %s, %s, %s, %s)
+                                       """, (etf['id'], stock['id'], current_date, shares, weight))
+                                       
+connection.commit()
+    
+    
---
+++ search_wsb.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 17 19:58:01 2021
+
+@author: User
+"""
+
+import datetime
+from psaw import PushshiftAPI
+import psycopg2
+import psycopg2.extras
+import config
+
+
+connection = psycopg2.connect(host =config.DB_HOST, database = config.DB_NAME, user = config.DB_USER, password = config.DB_PASS)
+cursor = connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
+cursor.execute(""" 
+               SELECT * FROM stock 
+               
+               """)
+rows = cursor.fetchall()
+
+
+stocks = {}
+for row in rows:
+    stocks['$' + row['symbol']] = row['id']
+    #print(row)
+    
+#print(stocks)
+
+api = PushshiftAPI()
+
+start_time = int(datetime.datetime(2021, 5, 1).timestamp())
+
+submissions = api.search_submissions(after=start_time,
+                                      subreddit='wallstreetbets',
+                                      filter=['url','author', 'title', 'subreddit'])
+
+
+for submission in submissions:
+    words = submission.title.split()
+    cashtags = list(set(filter(lambda word: word.lower().startswith('$'), words)))
+    
+    if (len(cashtags) > 0):
+        #print(cashtags)
+        #print (submission.created_utc)
+        #print (submission.title)
+        #print (submission.url)
+        
+        for cashtag in cashtags:
+            submitted_time = datetime.datetime.fromtimestamp(submission.created_utc).isoformat()
+            try:
+                cursor.execute("""
+                               INSERT INTO mention (dt, stock_id, message, sourse, url)
+                               VALUES (%s, %s, %s, 'wallstreetbets', %s)
+                               """, (submitted_time, stocks[cashtag], submission.title, submission.url))
+                connection.commit()
+            except Exception as e:
+                print(e)
+                connection.rollback()
